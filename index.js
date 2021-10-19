925f3533b2ce1d9084001ffe5e54606981785c19
MaxLyFe <85650989+maxlyfe@users.noreply.github.com>
2021-10-19 17:47:20 -0300

    atualizando acesso ao banco.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 
 const cors = require("cors");
@@ -11,12 +15,17 @@ app.use(cors());
 const TarefasRouters = require("./Routes/tarefas.routes");
 const Conn = require("./Conn/Conn");
 
-Conn();
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+Conn(db_url, db_user, db_pass, db_data);
 
 app.use("/tarefas", TarefasRouters);
 
 const port = 3001;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`App rodando.`);
 });
